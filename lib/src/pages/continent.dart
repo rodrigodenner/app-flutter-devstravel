38931a4bb0f4b3a2aa16f149fec746e179b6498f
rodrigodenner <rodrigodenner.art@gmail.com>
2024-05-09 21:28:54 -0300

    fix: continent.dart

--- lib/src/pages/continent.dart
+++ lib/src/pages/continent.dart
@@ -6,7 +6,7 @@ import '../partials/customdrawer.dart';
 import '../partials/citybox.dart';
 
 class ContinentPage extends StatelessWidget {
-  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
 
   TextStyle styles = TextStyle(
     fontSize: 15,
@@ -14,85 +14,85 @@ class ContinentPage extends StatelessWidget {
     fontFamily: 'Helvetica Neue',
   );
 
-  void seeCityAction(continentIndex){
+  void seeCityAction(int continentIndex) {
     print(continentIndex);
   }
 
-  void cityBoxAction(cityData){
+  void cityBoxAction(cityData) {
     print(cityData['name']);
   }
-}
 
-@override
-Widget build(BuildContext context) {
-  return Consumer<AppData>(
-    builder: (ctx, appdata, child) => Scaffold(
-      key: _scaffoldKey,
-      appBar: CustomAppBar(
-        pageContext: context,
-        scaffoldKey: _scaffoldKey,
-        title: 'Escolha um continente',
-        hideSearch: false,
-        showDrawer: true,
-      ),
-      drawer: CustomDrawer(
-        pageContext: context,
-      ),
-      backgroundColor: Colors.white,
-      body: ListView.builder(
-        itemCount: appdata.data.length,
-        itemBuilder: (context, index) {
-          var cities = [];
-          for (var country in appdata.data[index]['countries']) {
-            cities.addAll(country['cities']);
-          }
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AppData>(
+      builder: (ctx, appdata, child) => Scaffold(
+        key: _scaffoldKey,
+        appBar: CustomAppBar(
+          pageContext: context,
+          scaffoldKey: _scaffoldKey,
+          title: 'Escolha um continente',
+          hideSearch: false,
+          showDrawer: true,
+        ),
+        drawer: CustomDrawer(
+          pageContext: context,
+        ),
+        backgroundColor: Colors.white,
+        body: ListView.builder(
+          itemCount: appdata.data.length,
+          itemBuilder: (context, index) {
+            var cities = [];
+            for (var country in appdata.data[index]['countries']) {
+              cities.addAll(country['cities']);
+            }
 
-          return Column(
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.only(left: 15),
-                    child: Text(
-                      "${appdata.data[index]['name']} (${cities.length})",
-                      style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Helvetica Neue',
-                      ),
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: (){
-                      seeCityAction(index);
-                    },
-                    child: Text('Ver cidades',
+            return Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(left: 15),
+                      child: Text(
+                        "${appdata.data[index]['name']} (${cities.length})",
                         style: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 13,
+                          fontSize: 14,
                           fontWeight: FontWeight.bold,
                           fontFamily: 'Helvetica Neue',
-                        )),
-                  ),
-                ],
-              ),
-              Container(
-                height: 130,
-                margin: EdgeInsets.only(bottom: 15),
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: cities.length,
-                  itemBuilder: (cityContext, cityIndex) {
-                    return CityBox(
-                        data: cities[cityIndex], onTap: cityBoxAction);
-                  },
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        seeCityAction(index);
+                      },
+                      child: Text('Ver cidades',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 13,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Helvetica Neue',
+                          )),
+                    ),
+                  ],
                 ),
-              )
-            ],
-          );
-        },
+                Container(
+                  height: 130,
+                  margin: EdgeInsets.only(bottom: 15),
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: cities.length,
+                    itemBuilder: (cityContext, cityIndex) {
+                      return CityBox(
+                          data: cities[cityIndex], onTap: cityBoxAction);
+                    },
+                  ),
+                )
+              ],
+            );
+          },
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
